c35f7459e5fe796c8c4bae1e11a7f87eaf20f7b2
JiangengDong <dongjiangeng@gmail.com>
2018-12-23 14:05:33 +0800

    Rename variables and improve the loadMesh function

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+"use strict";
+
 // global
 let scene = new THREE.Scene(),
-    FPCamera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 1000),    // first-person camera
-    TPCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000),   // third-person camera
+    FPCamera = new THREE.PerspectiveCamera(50, 1, 0.01, 1000),    // first-person camera
+    TPCamera = new THREE.PerspectiveCamera(75, 1, 0.01, 1000),   // third-person camera
     renderer = new THREE.WebGLRenderer(),
     TPControl = new THREE.OrbitControls(TPCamera, document.getElementById("canvas-frame")),     // third-person camera is controlled with orbit control
     FPControl = new THREE.PointerLockControls(FPCamera),   // controlled with first-person control
@@ -19,7 +21,7 @@ let prevTime = performance.now();
 
 let stats;
 
-var speedControl = new function () {
+let speedControl = new function () {
     this.speed = 0;
 };
 
@@ -46,24 +48,21 @@ function init() {
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     // adjust canvas
     resize();
-    // parameters for camera control
+    // parameters for third-person camera control
     TPControl.screenSpacePanning = true;
     TPControl.minDistance = 5;
     TPControl.maxDistance = 100;
     TPControl.target.set(0, 0, 0);
+    TPControl.enableKeys = false;
     TPCamera.position.set(0, 20, 40);
+    TPCamera.name = "Third-Person Camera";
     TPControl.update();
+    // parameters for first-person camera control
     FPControl.getObject().position.z = 10;
+    FPCamera.name = "First-Person Camera";
     scene.add(FPControl.getObject());
-    // layers of camera
-    TPCamera.layers.set(0);
-    FPCamera.layers.set(1);
     // background color
     scene.background = new THREE.Color(0xffffff);
-    // elements should have name for debug
-    TPCamera.name = "Third-Person Camera";
-    FPCamera.name = "First-Person Camera";
-
     // FPS counter
     stats = new Stats();
     stats.domElement.style.position = 'absolute';
@@ -74,91 +73,67 @@ function init() {
 
 function buildScene() {
 
-    var gridHelper = new THREE.GridHelper( 1000, 20 );
-    scene.add( gridHelper );
+    let gridHelper = new THREE.GridHelper(1000, 20);
+    scene.add(gridHelper);
 
-    var shadowMaterial = new THREE.ShadowMaterial( { color: 0x000000 } );
-    shadowMaterial.opacity = 0.5;
-    var groundMesh = new THREE.Mesh(
-        new THREE.BoxGeometry( 100, .1, 100 ),
-        shadowMaterial
-    );
-    groundMesh.receiveShadow = true;
-    groundMesh.layers.mask = 0xffffffff;
-    scene.add( groundMesh );
+    let floor = drawFloor();
+    scene.add(floor);
 
     // cube
-    let cube = newCube(10, 10, 10, 0x00ff00);
+    let cube = drawBox(1, 1, 1, 0x00ff00);
+    cube.position.set(0, 5, 0);
     cube.animate = function () {
         this.rotation.x += 0.01;
         this.rotation.y += 0.01;
         this.rotation.z += 0.01;
     };
-    cube.castShadow = true;
-    cube.position.set(0, 5, 0);
-    cube.layers.mask = 0xffffffff;
     scene.add(cube);
 
-
-    let bed1 = Draw_Bed();
-    bed1.position.set (20, 0, 0);
-    bed1.scale.x = 0.2;
-    bed1.scale.y = 0.2;
-    bed1.scale.z = 0.2;
-    bed1.layers.mask = 0xffffffff;
-    bed1.layers.set(1);
+    let bed1 = drawBed();
+    bed1.position.set(20, 0, 0);
+    [bed1.scale.x, bed1.scale.y, bed1.scale.z] = [0.2, 0.2, 0.2];
     scene.add(bed1);
 
     // the man to be controlled
-    let man = loadMan();
-    man.scale.x = 0.1;
-    man.scale.y = 0.1;
-    man.scale.z = 0.1;
+    let man = loadMesh("resources/model/male02.obj", null, "man");
+    [man.scale.x, man.scale.y, man.scale.z] = [0.06, 0.06, 0.06];
     man.rotateY(Math.PI);
-    man.translateY(-8);
-    man.layers.mask = 0xffffffff;
-    man.layers.disable(1);
+    man.translateY(-10);
+    man.translateZ(-0.5);
     FPControl.getObject().add(man);
 
     // door
-    let door = loadDoor();
-    door.layers.mask = 0xffffffff;
+    let door = loadMesh("resources/model/Room-Door/Door_Component_BI3.obj",
+        "resources/model/Room-Door/Door_Component_BI3.mtl", "door");
     door.translateY(0);
     door.translateZ(-10);
-    door.scale.x = 10;
-    door.scale.y = 10;
-    door.scale.z = 10;
+    [door.scale.x, door.scale.y, door.scale.z] = [8, 8, 8];
     scene.add(door);
 
-    // var fan = new Fan();
-    // fan.threegroup.layers.mask = 0xffffffff;
-    // fan.threegroup.scale.x = 0.1;
-    // fan.threegroup.scale.y = 0.1;
-    // fan.threegroup.scale.z = 0.1;
-    // fan.threegroup.position.y = 10;
-    // fan.threegroup.position.x = -20;
-    // fan.threegroup.animate = function(){
-    //     fan.update();
-    // };
-    // scene.add(fan.threegroup);
+    // TODO: there is some problem with Fan, please fix it
+    // let fan = new Fan();
+    //     // fan.threegroup.layers.mask = 0xffffffff;
+    //     // fan.threegroup.scale.x = 0.1;
+    //     // fan.threegroup.scale.y = 0.1;
+    //     // fan.threegroup.scale.z = 0.1;
+    //     // fan.threegroup.position.y = 10;
+    //     // fan.threegroup.position.x = -20;
+    //     // fan.threegroup.animate = function(){
+    //     //     fan.update();
+    //     // };
+    //     // scene.add(fan.threegroup);
 
     // point light
-    light = newPointLight();
-    light.position.set(-50, 20, 100);
-    light.layers.mask = 0xffffffff;
-    light.castShadow = true;        //Set up shadow properties for the light
-    light.shadow.mapSize.width = 2048;
-    light.shadow.mapSize.height = 2048;
-    light.shadow.camera.near = 0.5;
-    light.shadow.camera.far = 500;
+    let light = newPointLight(1, 0xffffff);
+    light.position.set(-20, 50, 100);
     scene.add(light);
 
     // ambient light
-    light = newAmbientLight();
+    light = newAmbientLight(0.2, 0xffffff);
     scene.add(light);
 
-    var gui = new dat.GUI();
-    gui.add(speedControl, 'speed',0, 0.5).name("Speed");
+    let gui = new dat.GUI();
+    gui.add(speedControl, 'speed', 0, 0.5).name("Speed");
 }
 
 function resize() {
@@ -302,147 +277,151 @@ function onFullscreenChange(event) {
 }
 
 // basic function for adding objects
-function newCube(width = 1, height = 1, depth = 1, color = 0x00ff00) {
-    const geom = new THREE.CubeGeometry(width, height, depth);
-    const mat = new THREE.MeshLambertMaterial({color: color});
-    const cube = new THREE.Mesh(geom, mat);
-    cube.name = "cube";
-    return cube;
-}
-
-
-function newPointLight() {
-    const light = new THREE.PointLight(0xffffff, 1, 0, 2);
-    light.name = "point_light";
+function newPointLight(intensity = 1, color = 0xffffff, name = "pointLight") {
+    let light = new THREE.PointLight(color, intensity, 0, 2);
+    light.name = name;
+    light.castShadow = true;
+    light.shadow.mapSize.width = 2048;
+    light.shadow.mapSize.height = 2048;
+    light.shadow.camera.near = 0.5;
+    light.shadow.camera.far = 500;
     return light;
 }
 
-function newAmbientLight() {
-    const light = new THREE.AmbientLight(0xffffff, 0.2);
-    light.name = "ambient_light";
+function newAmbientLight(intensity = 0.2, color = 0xffffff, name = "ambientLight") {
+    let light = new THREE.AmbientLight(color, intensity);
+    light.name = name;
     return light;
 }
 
-function loadMan() {
-    let man = new THREE.Object3D();
+function loadMesh(modelUrl, materialURL, name = "meshObject") {
+    let obj = new THREE.Object3D();
     let loader = new THREE.OBJLoader2();
-    loader.load("resources/model/male02.obj",
-        function onLoad(event) {
-            man.add(event.detail.loaderRootNode);
-        });
-    man.name = "man";
-    return man;
-}
-
-function loadDoor() {
-    let door = new THREE.Object3D();
-    //scene.add(texture);
-    let Loader = new THREE.OBJLoader2();
-    Loader.loadMtl('resources/model/Room-Door/Door_Component_BI3.mtl', null,
-        function onLoadMtl(materials) {
-            Loader.setMaterials(materials);
-            Loader.setModelName('door');
-            Loader.load('resources/model/Room-Door/Door_Component_BI3.obj',
-                function onLoad(event) {
-                    event.detail.loaderRootNode.traverse((child) => {
-                        child.layers.mask = 0xffffffff;
+    if (materialURL) {
+        loader.loadMtl(materialURL, null,
+            function onLoadMtl(materials) {
+                loader.setMaterials(materials);
+                loader.load(modelUrl,
+                    function onLoad(event) {
+                        obj.add(event.detail.loaderRootNode);
+                        for (let mesh of event.detail.loaderRootNode.children) {
+                            mesh.receiveShadow = true;
+                            mesh.castShadow = true;
+                        }
                     });
-                    door.add(event.detail.loaderRootNode);
-                });
-        });
-    door.name = "door";
-    return door;
+            });
+    } else {
+        loader.load(modelUrl,
+            function onLoad(event) {
+                obj.add(event.detail.loaderRootNode);
+                for (let mesh of event.detail.loaderRootNode.children) {
+                    mesh.receiveShadow = true;
+                    mesh.castShadow = true;
+                }
+            });
+    }
+    obj.name = name;
+    obj.castShadow = true;
+    obj.receiveShadow = true;
+    return obj;
 }
 
 function importMesh() {
     let file = document.getElementById("file_path").files[0];
-    let obj = new THREE.Object3D();
-    let loader = new THREE.OBJLoader2();
-    loader.load(URL.createObjectURL(file),
-        function onLoad(object) {
-            obj.add(object);
-        });
+    let obj = loadMesh(URL.createObjectURL(file), null, "objectImported");
     scene.add(obj);
 }
 
-function Draw_Pillar (ball_size, cylinder_height) {
-    const ball = new THREE.Mesh(
-        new THREE.OctahedronGeometry(ball_size,2),
-        new THREE.MeshStandardMaterial( {
+function drawFloor() {
+    let shadowMaterial = new THREE.ShadowMaterial({color: 0x000000});
+    shadowMaterial.opacity = 0.5;
+    let groundMesh = new THREE.Mesh(
+        new THREE.BoxGeometry(1000, .1, 1000),
+        shadowMaterial
+    );
+    groundMesh.receiveShadow = true;
+    groundMesh.name = "floor";
+    return groundMesh;
+}
+
+function drawPillar(ball_size, cylinder_height) {
+    let ball = new THREE.Mesh(
+        new THREE.OctahedronGeometry(ball_size, 2),
+        new THREE.MeshStandardMaterial({
             color: 0x72bdbf,
-            shading: THREE.FlatShading ,
+            shading: THREE.FlatShading,
             metalness: 0,
             roughness: 0.8,
             refractionRatio: 0.25
-        } )
+        })
     );
     ball.position.y += cylinder_height;
     ball.castShadow = true;
     ball.receiveShadow = true;
-    ball.rotateZ(Math.random()*Math.PI*2);
-    ball.rotateY(Math.random()*Math.PI*2);
+    ball.rotateZ(Math.random() * Math.PI * 2);
+    ball.rotateY(Math.random() * Math.PI * 2);
 
-    const cylinder = new THREE.Mesh(
-        new THREE.CylinderGeometry( 5, 5, cylinder_height, 32 ),
-        new THREE.MeshStandardMaterial( {
+    let cylinder = new THREE.Mesh(
+        new THREE.CylinderGeometry(5, 5, cylinder_height, 32),
+        new THREE.MeshStandardMaterial({
             color: 0xf8db08,
-            shading: THREE.FlatShading ,
+            shading: THREE.FlatShading,
             metalness: 0,
             roughness: 0.8,
             refractionRatio: 0.25
-        } )
+        })
     );
-    cylinder.position.y = cylinder_height/2;
+    cylinder.position.y = cylinder_height / 2;
     cylinder.castShadow = true;
     cylinder.receiveShadow = true;
 
-    const pillar = new THREE.Group();
+    let pillar = new THREE.Group();
     pillar.add(ball);
     pillar.add(cylinder);
 
     return pillar;
 }
 
-function Draw_Box(x,y,z) {
-    const box = new THREE.Mesh(
-        new THREE.BoxGeometry( x,y,z ),
-        new THREE.MeshStandardMaterial( {
-            color: 0x3CB371,
-            shading: THREE.FlatShading ,
+function drawBox(x, y, z, color = 0x3CB371) {
+    let box = new THREE.Mesh(
+        new THREE.BoxGeometry(x, y, z),
+        new THREE.MeshStandardMaterial({
+            color: color,
+            shading: THREE.FlatShading,
             metalness: 0,
             roughness: 0.8,
             refractionRatio: 0.25
-        } )
+        })
     );
     box.castShadow = true;
     box.receiveShadow = true;
-
+    box.name = "box";
     return box;
 }
 
-function Draw_Bed() {
-    const pillar1 = Draw_Pillar(6,50);
+function drawBed() {
+    let pillar1 = drawPillar(6, 50);
     pillar1.position.x -= 30;
 
-    const pillar2 = Draw_Pillar(6,50);
+    let pillar2 = drawPillar(6, 50);
     pillar2.position.x += 30;
 
-    const pillar3 = Draw_Pillar(6,30);
+    let pillar3 = drawPillar(6, 30);
     pillar3.position.x += 30;
     pillar3.position.z += 100;
 
-    const pillar4 = Draw_Pillar(6,30);
+    let pillar4 = drawPillar(6, 30);
     pillar4.position.x -= 30;
     pillar4.position.z += 100;
 
-    const board1 = Draw_Box(60,40,5);
+    let board1 = drawBox(60, 40, 5);
     board1.position.y += 20;
 
-    const box1 = Draw_Box(60,20,100);
+    let box1 = drawBox(60, 20, 100);
     box1.position.y += 15;
     box1.position.z += 50;
 
-    const bed = new THREE.Group();
+    let bed = new THREE.Group();
     bed.add(pillar1);
     bed.add(pillar2);
     bed.add(pillar3);
@@ -453,50 +432,51 @@ function Draw_Bed() {
     return bed;
 }
 
-Fan = function (){
+let Fan;
+Fan = function () {
     this.speed = 0;
     this.acc = 0;
-    this.wind=false;
-    this.amp=0;
+    this.wind = false;
+    this.amp = 0;
     this.greenMat = new THREE.MeshLambertMaterial({
         color: 0x698B22,
-        shading:THREE.FlatShading
+        shading: THREE.FlatShading
     });
 
-    this.redMat = new THREE.MeshLambertMaterial ({
+    this.redMat = new THREE.MeshLambertMaterial({
         color: 0xad3525,
-        shading:THREE.FlatShading
+        shading: THREE.FlatShading
     });
-    this.greyMat = new THREE.MeshLambertMaterial ({
+    this.greyMat = new THREE.MeshLambertMaterial({
         color: 0x653f4c,
-        shading:THREE.FlatShading
+        shading: THREE.FlatShading
     });
 
-    this.yellowMat = new THREE.MeshLambertMaterial ({
+    this.yellowMat = new THREE.MeshLambertMaterial({
         color: 0xfdd276,
-        shading:THREE.FlatShading
+        shading: THREE.FlatShading
     });
 
-    this.blueMat = new THREE.MeshLambertMaterial ({
+    this.blueMat = new THREE.MeshLambertMaterial({
         color: 0x1E90FF,
-        shading:THREE.FlatShading,
+        shading: THREE.FlatShading,
         transparent: true,
         opacity: 0.5
     });
 
-    var torusGeom = new THREE.TorusGeometry(11,1.5,6,15,6.3);
-    var coneGeom = new THREE.ConeGeometry(10,100,7);
-    var coreGeom = new THREE.CylinderGeometry( 10, 10, 20, 32 );
-    var sphereGeom = new THREE.BoxGeometry(10, 10, 3);
-    var propGeom = new THREE.BoxGeometry(10,30,2);
-    propGeom.applyMatrix( new THREE.Matrix4().makeTranslation( 0,25,0) );
-    var scripGeom = new THREE.BoxGeometry(30,5,2);
-    scripGeom.applyMatrix( new THREE.Matrix4().makeTranslation( 15, 0, 0 ) );
+    let torusGeom = new THREE.TorusGeometry(11, 1.5, 6, 15, 6.3);
+    let coneGeom = new THREE.ConeGeometry(10, 100, 7);
+    let coreGeom = new THREE.CylinderGeometry(10, 10, 20, 32);
+    let sphereGeom = new THREE.BoxGeometry(10, 10, 3);
+    let propGeom = new THREE.BoxGeometry(10, 30, 2);
+    propGeom.applyMatrix(new THREE.Matrix4().makeTranslation(0, 25, 0));
+    let scripGeom = new THREE.BoxGeometry(30, 5, 2);
+    scripGeom.applyMatrix(new THREE.Matrix4().makeTranslation(15, 0, 0));
 
     this.torus = new THREE.Mesh(torusGeom, this.greenMat);
     this.torus.castShadow = true;
     this.torus.receiveShadow = true;
-    this.core = new THREE.Mesh(coreGeom,this.greyMat);
+    this.core = new THREE.Mesh(coreGeom, this.greyMat);
     this.core.castShadow = true;
     this.core.receiveShadow = true;
     this.cone = new THREE.Mesh(coneGeom, this.yellowMat);
@@ -504,16 +484,16 @@ Fan = function (){
     this.cone.receiveShadow = true;
 
     // propellers
-    var prop1 = new THREE.Mesh(propGeom, this.redMat);
+    let prop1 = new THREE.Mesh(propGeom, this.redMat);
     prop1.castShadow = true;
     prop1.receiveShadow = true;
     prop1.position.z = 15;
-    var prop2 = prop1.clone();
-    prop2.rotation.z = Math.PI/2;
-    var prop3 = prop1.clone();
+    let prop2 = prop1.clone();
+    prop2.rotation.z = Math.PI / 2;
+    let prop3 = prop1.clone();
     prop3.rotation.z = Math.PI;
-    var prop4 = prop1.clone();
-    prop4.rotation.z = -Math.PI/2;
+    let prop4 = prop1.clone();
+    prop4.rotation.z = -Math.PI / 2;
 
     this.scrip = [];
 
@@ -521,7 +501,7 @@ Fan = function (){
     this.scrip1.position.x = -19;
     this.scrip1.position.y = 50;
     this.scrip1.position.z = 22;
-    this.scrip1.rotation.z = -Math.PI/2;
+    this.scrip1.rotation.z = -Math.PI / 2;
     this.scrip2 = this.scrip1.clone();
     this.scrip2.position.x = 0;
     this.scrip2.position.y = 53;
@@ -542,7 +522,7 @@ Fan = function (){
     this.torus.scale.x = 5;
     this.torus.scale.y = 5;
     this.torus.scale.z = 5;
-    this.core.rotation.x =  Math.PI/2;
+    this.core.rotation.x = Math.PI / 2;
     this.cone.position.y = -50;
 
     this.propeller = new THREE.Group();
@@ -567,16 +547,15 @@ Fan = function (){
     this.threegroup.name = "fan";
 }
 
-Fan.prototype.update = function(){
+Fan.prototype.update = function () {
 
 
-    this.threegroup.position.z=0;
+    this.threegroup.position.z = 0;
 
-    if (this.speed<speedControl.speed) {
+    if (this.speed < speedControl.speed) {
         this.acc += 0.0001;
         this.speed += this.acc;
-    }
-    else if (this.speed>speedControl.speed){
+    } else if (this.speed > speedControl.speed) {
         this.acc = 0;
         this.amp = 0;
         this.speed *= .98;
@@ -584,8 +563,8 @@ Fan.prototype.update = function(){
     this.propeller.rotation.z += this.speed;
 
 
-    for (var i = 0; i < this.scrip.length; i++) {
-        this.scrip[i].rotation.x = -Math.PI*this.speed+Math.random()*this.speed;
+    for (let i = 0; i < this.scrip.length; i++) {
+        this.scrip[i].rotation.x = -Math.PI * this.speed + Math.random() * this.speed;
     }
 };
 
